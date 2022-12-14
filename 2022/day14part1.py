ca5c5bf7abf41c8710bea9fd4cb27544db204f87
Randy Poe <randall.poe.us@ieee.org>
2022-12-14 16:41:08 -0500

    Advent of Code, Day 14 solutions

---
+++ 2022/day14part1.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2022, Day 14, Part 1.
+
+Pour grains of sand into a rock formation until it is full and sand starts
+falling off the edige.
+
+
+Created on Wed Dec 14 05:56:30 2022
+
+@author: rpoepa
+"""
+import time
+import numpy as np
+
+tic = time.perf_counter()
+Infinity = 99999  # Use for "impossibly large integer"
+# First read in the rock paths
+rockpaths = []
+with open('input/input.2022day14.txt', 'r') as f:
+#with open('input/test.2022day14.txt', 'r') as f:
+    for line in f:
+        pathstr = line.strip().replace('->',' ').split()
+        path = []
+        for pair in pathstr:
+            xy = pair.split(',')
+            path.append([int(xy[0]), int(xy[1])])
+        rockpaths.append(path)
+
+# Determine limits of the grid
+xmin = Infinity
+xmax = -Infinity
+ymin = Infinity
+ymax = -Infinity
+for path in rockpaths:
+    for xy in path:
+        xmin = min(xmin, xy[0])
+        xmax = max(xmax, xy[0])
+        ymin = min(ymin, xy[1])
+        ymax = max(ymax, xy[1])
+
+ymin = 0   # Force extra rows at the top
+
+# Convenience function for coordinate transformation
+xy_to_ij = lambda xy: (xy[1] - ymin, xy[0] - xmin)
+    
+# Build grid and trace paths
+width = xmax - xmin + 1
+height = ymax - ymin + 1
+grid = [['.']*width for _ in range(height)]
+for path in rockpaths:
+    ij0 = xy_to_ij(path[0])
+    for point in path[1:]:
+        ij1 = xy_to_ij(point)
+        di = np.sign(ij1[0] - ij0[0])  # One of these is always 0
+        dj = np.sign(ij1[1] - ij0[1])
+        i = ij0[0]
+        j = ij0[1]
+        while i != ij1[0] or j != ij1[1]:
+            grid[i][j] = '#'
+            i += di
+            j += dj
+        grid[ij1[0]][ij1[1]] = '#'  # Add the endpoint
+        ij0 = ij1
+
+
+#for row in grid:
+#    print(''.join(row))
+toc = time.perf_counter()
+
+print(f'Time to build map = {(toc - tic) * 1000} ms')
+
+toc = time.perf_counter()
+# Begin dropping sand.
+into_the_abyss = False
+grains = 0
+while not into_the_abyss:
+    # Create a new grain of sand
+    (i, j) = xy_to_ij([500, 0])
+    grains += 1
+    blocked = False
+    
+    # Dropping logic
+    while not blocked or into_the_abyss:
+        i += 1
+        if i >= height:
+            into_the_abyss = True
+            break
+        if grid[i][j] == '.':
+            continue
+
+        # Blocked below. Try left.            
+        if j == 0:
+            into_the_abyss = True
+            break
+        if grid[i][j-1] =='.':
+            j -= 1
+            continue
+        
+        # Blocked left. Try right.
+        if j >= width - 1:
+            into_the_abyss = True
+            break
+        
+        if grid[i][j+1] == '.': 
+            j += 1
+            continue
+        
+        grid[i-1][j] = 'o'
+        blocked = True
+
+grains -= 1  # That last one that fell into the abyss
+toc2 = time.perf_counter()
+print(f'Time to drop {grains} grains of sand = {toc2 - toc} s')
+
+for row in grid:
+    print(''.join(row))
+            
